6c689ce42fdc47dcc6db4ea53533608533e2f7d7
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2025-12-23 18:42:22 +0530

    Add comment controller with create and retrieve functionality

---
+++ comments/controller/comment.js
@@ -0,0 +1,31 @@
+import { randomBytes } from "crypto";
+import { commentsDB } from "../database/index.js";
+
+export const createComment = (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+
+  const { text } = req.body;
+
+  const snippetId = req.params.id;
+
+  const comments = commentsDB[snippetId] || [];
+
+  // create comments
+  comments.push({ commentId, text });
+
+  commentsDB[snippetId] = comments;
+
+  return res.status(201).json({
+    success: true,
+    message: "Comment added",
+    comment: { commentId, text },
+  });
+};
+
+export const getCommentBySnippetId = (req, res) => {
+  const snippetId = req.params.id;
+
+  console.log(snippetId);
+
+  return res.status(200).json(commentsDB[snippetId] || []);
+};
